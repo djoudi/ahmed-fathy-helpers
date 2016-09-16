78ef394ffb90a915a9d722d5d06f3fe8f8a1a708
afza2013 <alfurat.eg@gmail.com>
2016-09-16 16:23:55 +0200

    Update unauthorized.blade.php

--- src/Views/unauthorized.blade.php
+++ src/Views/unauthorized.blade.php
@@ -18,34 +18,8 @@
         <meta content="" name="author" />
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
         {!! Html::style('http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all') !!}
-        {!! Html::style($cpanel.'css/font-awesome.min.css') !!}
-        {!! Html::style($cpanel.'css/simple-line-icons.min.css') !!}
-        {!! Html::style($cpanel.'css/bootstrap'.getDir(true).'.min.css') !!}
-        {!! Html::style($cpanel.'css/uniform.default.min.css') !!}
-        {!! Html::style($cpanel.'css/bootstrap-switch'.getDir(true).'.min.css') !!}
-        <!-- END GLOBAL MANDATORY STYLES -->
-        <!-- BEGIN PAGE LEVEL PLUGINS -->
-       {!! Html::style($cpanel.'css/daterangepicker.min.css') !!}
-        {!! Html::style($cpanel.'css/morris.css') !!}
-        {!! Html::style($cpanel.'css/fullcalendar.min.css') !!}
-        {!! Html::style($cpanel.'css/jqvmap.css') !!}
-        <!-- END PAGE LEVEL PLUGINS -->
-        <!-- BEGIN THEME GLOBAL STYLES -->
-
-       {!! Html::style($cpanel.'css/components'.getDir(true).'.min.css') !!}
-        {!! Html::style($cpanel.'css/plugins'.getDir(true).'.min.css') !!}
-        {!! Html::style($cpanel.'css/error'.getDir(true).'.min.css') !!}
-        <!-- END THEME GLOBAL STYLES -->
-        <!-- BEGIN THEME LAYOUT STYLES -->
-        {!! Html::style($cpanel.'css/layout'.getDir(true).'.min.css') !!}
-        {!! Html::style($cpanel.'css/darkblue'.getDir(true).'.min.css') !!}
-        {!! Html::style($cpanel.'css/custom'.getDir(true).'.min.css') !!}
-        {!! Html::style($cpanel.'css/datatables.min.css') !!}
-        {!! Html::style($cpanel.'css/datatables.bootstrap'.getDir(true).'.css') !!}
-        {!! Html::style($cpanel.'css/bootstrap-fileinput.css') !!}
-        {!! Html::style($cpanel.'css/summernote.css') !!}
-        {!! Html::style($cpanel.'css/icheck/all.css') !!}
-        {!! Html::style($cpanel.'css/my-style.css') !!}
+        
+        {!! Html::style($cpanel.'css/style-'.getDir().'.css') !!}
 
         <!-- END THEME LAYOUT STYLES -->
         <link rel="shortcut icon" href="{{ $icon }}" /> </head>
@@ -66,8 +40,7 @@
     </div>
 </div>
        <!--[if lt IE 9]>
-        {!! Html::script($cpanel.'/js/respond.min.js') !!}
-        {!! Html::script($cpanel.'/js/excanvas.min.js') !!}
+        {!! Html::script($cpanel.'/js/script.js') !!}
 <![endif]-->
 
     </body>
