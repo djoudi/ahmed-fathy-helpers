5f0bb994ee230863558c4fcde5fea42e62dc0021
afza2013 <alfurat.eg@gmail.com>
2016-09-18 11:46:41 +0200

    Update breadcrumbs.php

--- src/helpers/src/breadcrumbs.php
+++ src/helpers/src/breadcrumbs.php
@@ -1,40 +1,48 @@
 <?php
-Breadcrumbs::register('cp.home', function($breadcrumbs){
-    $breadcrumbs->push(trans('lang.home'), url(cp),['icon'=>'fa fa-home']);
-});
+try {
+	Breadcrumbs::register('cp.home', function($breadcrumbs){
+	    $breadcrumbs->push(trans('lang.home'), url(cp),['icon'=>'fa fa-home']);
+	});
 
 
-Breadcrumbs::register('cp.settings', function($breadcrumbs){
-    $breadcrumbs->parent('cp.home');
-    $breadcrumbs->push(trans('lang.settings'), route('main.settings'),['icon'=>'icon-settings']);
-});
+	Breadcrumbs::register('cp.settings', function($breadcrumbs){
+	    $breadcrumbs->parent('cp.home');
+	    $breadcrumbs->push(trans('lang.settings'), route('main.settings'),['icon'=>'icon-settings']);
+	});
 
+} catch (\Exception $e) {
+	
+}
 function setBreadcrumbs($name,$url,$parent = 'cp.home')
 {
-	if (is_array($name)) 
-	{
-		foreach ($name as $key => $trans) 
+	try {
+		if (is_array($name)) 
 		{
-			Breadcrumbs::register($key, function($breadcrumbs) use($key,$trans,$url,$parent){
+			foreach ($name as $key => $trans) 
+			{
+				Breadcrumbs::register($key, function($breadcrumbs) use($key,$trans,$url,$parent){
+				    !is_null($parent) ? $breadcrumbs->parent($parent) : '';
+				    $breadcrumbs->push($trans, $url);
+				});
+			}
+		}else{
+			Breadcrumbs::register($name, function($breadcrumbs) use($name,$url,$parent){
 			    !is_null($parent) ? $breadcrumbs->parent($parent) : '';
-			    $breadcrumbs->push($trans, $url);
+			    $breadcrumbs->push(trans('lang.'.$name), $url);
 			});
 		}
-	}else{
-		Breadcrumbs::register($name, function($breadcrumbs) use($name,$url,$parent){
-		    !is_null($parent) ? $breadcrumbs->parent($parent) : '';
-		    $breadcrumbs->push(trans('lang.'.$name), $url);
-		});
+	} catch (\Exception $e) {
+		
 	}
 	
 }
 
 function resourceBreadcrumbs($name , $colum , $urls = [],$cp = true,$customName = null)
 {
+try {
 	$indexUrl = isset($urls['index']) ? $urls['index'] : route(str_plural($name).'.index');
 	$createUrl = isset($urls['create']) ? $urls['create'] : route(str_plural($name).'.create');
 	$model = '\App\\'.ucfirst(str_singular(camel_case($name)));
-
 	Breadcrumbs::register(str_plural($name), function($breadcrumbs) use($name,$indexUrl,$cp,$customName){
 	    $breadcrumbs->parent(($cp ? 'cp.home' : 'home'));
 	    $trans = !is_null($customName) ? $customName : trans('lang.'.str_plural($name));
@@ -74,6 +82,10 @@ function resourceBreadcrumbs($name , $colum , $urls = [],$cp = true,$customName 
 	    $breadcrumbs->push(trans('lang.edit'), $editUrl);
 	});
 
+} catch (\Exception $e) {
+	
+}
+	
 }
 
 
